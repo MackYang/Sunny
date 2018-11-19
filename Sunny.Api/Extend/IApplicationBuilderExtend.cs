718ab39b8be5360a8212d2fb21628ba66a815db0
Mack.Yang <1651493066@qq.com>
2018-11-19 11:33:02 +0800

    优化了任务调度

--- Sunny.Api/Extend/IApplicationBuilderExtend.cs
+++ Sunny.Api/Extend/IApplicationBuilderExtend.cs
@@ -27,46 +27,44 @@ namespace Microsoft.AspNetCore.Builder
         /// <param name="schedulerFactory"></param>
         static public async void EnableJob(this IApplicationBuilder builder, IConfiguration configuration, ISchedulerFactory schedulerFactory)
         {
-            //1、通过调度工厂获得调度器
-            var scheduler = await schedulerFactory.GetScheduler();
-            //2、开启调度器
-            await scheduler.Start();
-
             var option = configuration.GetSection("SunnyOptions:JobOption").Get<JobOption[]>();
-
-
-            var types = DiHelper.GetCustomizeTypes(t => typeof(IJobEntity).IsAssignableFrom(t) && !t.GetTypeInfo().IsAbstract);
-
-
-
-            foreach (var item in option)
+            if (option != null && option.Length > 0)
             {
-                var tArr = types.Where(x => x.Name == item.JobClassName);
-                foreach (var t in tArr)
+                //1、通过调度工厂获得调度器
+                var scheduler = await schedulerFactory.GetScheduler();
+                //2、开启调度器
+                await scheduler.Start();
+
+                var types = DiHelper.GetCustomizeTypes(t => typeof(IJobEntity).IsAssignableFrom(t) && !t.GetTypeInfo().IsAbstract);
+
+
+                foreach (var item in option)
                 {
-                    //3、创建一个触发器
-                    var trigger = TriggerBuilder.Create()
-                                    .WithCronSchedule(item.RunAtCron)
-                                    .Build();
+                    var tArr = types.Where(x => x.Name == item.JobClassName);
+                    foreach (var t in tArr)
+                    {
+                        //3、创建一个触发器
+                        var trigger = TriggerBuilder.Create()
+                                        .WithCronSchedule(item.RunAtCron)
+                                        .Build();
 
-                    var wrapperType = typeof(JobWrapper<>).MakeGenericType(t.GetTypeInfo());
+                        var wrapperType = typeof(JobWrapper<>).MakeGenericType(t.GetTypeInfo());
 
-                    //4、创建任务
-                    var jobDetail = JobBuilder.Create(wrapperType)
-                                    .WithIdentity(item.JobClassName, item.JobGroup)
-                                  .SetJobData(item.Args == null ? new JobDataMap() : new JobDataMap(item.Args))
-                                    .Build();
+                        //4、创建任务
+                        var jobDetail = JobBuilder.Create(wrapperType)
+                                        .WithIdentity(item.JobClassName, item.JobGroup)
+                                      .SetJobData(item.Args == null ? new JobDataMap() : new JobDataMap(item.Args))
+                                        .Build();
 
 
-                    //5、将触发器和任务器绑定到调度器中
-                    await scheduler.ScheduleJob(jobDetail, trigger);
+                        //5、将触发器和任务器绑定到调度器中
+                        await scheduler.ScheduleJob(jobDetail, trigger);
+
+                    }
+
 
                 }
-
-
             }
-
-
 
         }
     }
